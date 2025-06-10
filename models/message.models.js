84ee5644fb417889f1648b44c1721e6d73b4062a
ajajalam <alamajaj813@gmail.com>
2025-06-10 13:44:28 +0530

    second commit

---
+++ models/message.models.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    messageContent: {
+        type: String,
+        required: true,
+        maxlength: 2000
+    },
+    messageType: {
+        type: String,
+        enum: ['text', 'image', 'file'],
+        default: 'text'
+    },
+    status: {
+        type: String,
+        enum: ['sent', 'delivered', 'read'],
+        default: 'sent'
+    },
+    attachmentUrl: {
+        type: String // Optional for image or file message types
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const Message = mongoose.model('Message', MessageSchema);
+module.exports = Message;
---
+++ models/orders.models.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    customer: {
+        type: String, 
+        required: true,
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+    },
+    productName: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['pending', 'paid', 'failed', 'refunded'],
+        default: 'pending'
+    },
+    oredrTime:{
+        type: <time datetime="time"></time>   
+    },
+    totalAmount: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    shippingAddress: {
+        street: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true }
+    },
+    orderDate: {
+        type: Date,
+        default: Date.now
+    },
+    deliveryDate: {
+        type: Date
+    },
+    trackingNumber: {
+        type: String
+    },
+    isCancelled: {
+        type: Boolean,
+        default: false
+    },
+    cancellationReason: {
+        type: String
+    }
+}, {
+    timestamps: true // Adds createdAt and updatedAt automatically
+});
+
+const Order = mongoose.model('Order', OrderSchema);
+module.exports = Order;
+
